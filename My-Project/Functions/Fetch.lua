1031a5e2fdeaec5c3805f5814c86e21805dee2d0
Proxides <94020472+Proxides@users.noreply.github.com>
2022-04-11 22:11:44 -0400

    Delete My-Project directory

--- My-Project/Functions/Fetch.lua
+++
@@ -1,8 +0,0 @@
---Simply fetches my github repositories, if you wish to use this function just replace "Proxides" with your github user.
-
-return function(Obj)
-    assert(Obj, "No arguments were passed!")
-    
-    Obj = Obj:gsub("/", "/main/")
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Proxides/" .. Obj, true))()
-end
