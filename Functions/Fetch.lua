4098aa461708e608a2381e72e902e8fb9f8c2bc3
HarmonicDust <94020472+HarmonicDust@users.noreply.github.com>
2022-04-19 23:25:17 -0400

    Update Fetch.lua

--- Functions/Fetch.lua
+++ Functions/Fetch.lua
@@ -1,8 +1,8 @@
---Simply fetches my github repositories, if you wish to use this function just replace "Proxides" with your github user.
+--Simply fetches my github repositories, if you wish to use this function just replace "HarmonicDust" with your github user.
 
 return function(Obj)
     assert(Obj, "No arguments were passed!")
     
     Obj = Obj:gsub("/", "/main/"):gsub(":", "/")
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Proxides/" .. Obj, true))()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/HarmonicDust/" .. Obj, true))()
 end
